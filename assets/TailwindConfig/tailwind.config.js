86ed1136026c15e399a375409d481efb915b0e77
zohirshoh-ahatov <xolmurodovvohid20@gmail.com>
2024-05-22 23:24:04 +0500

    header and hero section done responsive

---
+++ assets/TailwindConfig/tailwind.config.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+    theme: {
+        screens: {
+            lg1240: { 'max': '1240px' },
+            lg1010: { 'max': '1010px' },
+            lg910: { 'max': '910px' },
+            md874: { 'max': '874px' },
+            md750: { 'max': '750px' },
+            md670: { 'max': '670px' },
+            sm430: { 'max': '430px' },
+            sm375: { 'max': '375px' },
+        },
+    }
+}
