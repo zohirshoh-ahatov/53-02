9c9308209f6b51f52003951b009762b49b9c988f
zohirshoh-ahatov <xolmurodovvohid20@gmail.com>
2024-05-23 23:19:48 +0500

    Ready to view on mobile phone!

--- assets/TailwindConfig/tailwind.config.js
+++ assets/TailwindConfig/tailwind.config.js
@@ -2,15 +2,24 @@ tailwind.config = {
     theme: {
         screens: {
             lg1240: { 'max': '1240px' },
+            lg1170: { 'max': '1170px' },
+            lg1080: { 'max': '1080px' },
+            lg1060: { 'max': '1060px' },
             lg1024: { 'max': '1024px' },
             lg1010: { 'max': '1010px' },
             lg910: { 'max': '910px' },
+            md898: { 'max': '898px' },
             md874: { 'max': '874px' },
             md820: { 'max': '820px' },
             md750: { 'max': '750px' },
+            md726: { 'max': '726px' },
             md670: { 'max': '670px' },
+            sm500: { 'max': '500px' },
+            sm452: { 'max': '452px' },
             sm430: { 'max': '430px' },
+            sm408: { 'max': '408px' },
             sm375: { 'max': '375px' },
+            sm368: { 'max': '368px' },
         },
     }
 }
