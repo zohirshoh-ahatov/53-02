fb59ef36c8f69fc189cc3cc33a8858fd24ef755c
zohirshoh-ahatov <xolmurodovvohid20@gmail.com>
2024-05-23 12:35:46 +0500

    second section done

--- assets/TailwindConfig/tailwind.config.js
+++ assets/TailwindConfig/tailwind.config.js
@@ -2,9 +2,11 @@ tailwind.config = {
     theme: {
         screens: {
             lg1240: { 'max': '1240px' },
+            lg1024: { 'max': '1024px' },
             lg1010: { 'max': '1010px' },
             lg910: { 'max': '910px' },
             md874: { 'max': '874px' },
+            md820: { 'max': '820px' },
             md750: { 'max': '750px' },
             md670: { 'max': '670px' },
             sm430: { 'max': '430px' },
